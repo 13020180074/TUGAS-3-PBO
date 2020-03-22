aeb99c387ff901892c41994b437be299bd8524b5
13020180074 <62000267+13020180074@users.noreply.github.com>
2020-03-22 08:31:51 -0700

    Add files via upload

---
+++ tugas3 pbo/asgdll.java
@@ -0,0 +1,17 @@
+public class asgdll {
+
+	/**
+	* @param args
+	*/
+	public static void main(String [] args) {
+		// TODO Auto-generated method sub
+		/* kamus */
+		float f= 20.0f;
+		double fll;
+		
+		/* Algoritma */
+		fll=10.0f;
+		System.out.println ("f : "+f +
+ "\nfll : "+fll);
+	}
+}
